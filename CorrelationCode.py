cb8e1a4dd5bf5f1d9050678d823b1d004310cb2c
ec221020 <ec221020@qmul.ac.uk>
2023-12-01 16:00:46 +0000

    *Changes to UnitTest

--- CorrelationCode.py
+++ CorrelationCode.py
@@ -50,23 +50,26 @@ y = violent_crimes
 
 test_result = unittest.TextTestRunner().run(unittest.TestLoader().loadTestsFromModule(Testing.TestFunction1))
 
+# Plotting
+plt.figure()
+plt.plot(video_gamer, violent_crimes, 'o')
+
 if test_result.wasSuccessful():
-    # Linear regression
+    # Linear regression if the test is successful
     lin_reg_m_main, lin_reg_q_main = linear_regression(video_gamer, violent_crimes)
-
-    # Calculate regression line
     y_main = lin_reg_m_main * video_gamer + lin_reg_q_main
-
-    # Plotting
-    plt.figure()
-    plt.plot(video_gamer, violent_crimes, 'o', label="Data points")
     plt.plot(video_gamer, y_main, label="Regression line")
+    # Calculate and display PMCC
+    plt.text(0.75, 1.035, f"PMCC: {PmCc(video_gamer, violent_crimes):.2f} > Successful Correlation", transform=plt.gca().transAxes, fontsize=5, verticalalignment='top')
+else:
+    # Display a message if the test is not successful
+    plt.text(0.75, 1.035, f"PMCC: {PmCc(video_gamer, violent_crimes):.2f} > No Successful Correlation", transform=plt.gca().transAxes, fontsize=5, verticalalignment='top')
 
-    plt.xlabel("Video gaming rate (%)")
-    plt.ylabel("Weapon possessions")
-    plt.title("Adult video gaming and weapon possessions correlation in the UK from 2007-2022")
+plt.xlabel("Video gaming rate (%)")
+plt.ylabel("Weapon possessions")
+plt.title("Adult video gaming and weapon possessions correlation")
 
-    plt.legend()
+plt.legend()
 
-    # Display the plot
-    plt.show()
+# Display the plot
+plt.show()
