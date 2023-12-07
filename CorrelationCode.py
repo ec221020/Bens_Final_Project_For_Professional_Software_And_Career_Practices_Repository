3333037e4ffb1a7dd15a02b985f7024e63c998c8
ec221020 <ec221020@qmul.ac.uk>
2023-12-07 14:11:49 +0000

    *new additions

--- CorrelationCode.py
+++ CorrelationCode.py
@@ -5,32 +5,142 @@ import pandas as pd
 import plotly.express as px
 import unittest
 import Testing
-from variables import video_gamer,violent_crimes
+from variables import video_gamer, gamingtwelve_rate, data_dates, violent_crimes, sexual_offences, weapon_possession,suicide_rate,homicide_rate
 from functions import PmCc,linear_regression
 
 x = video_gamer
-y = violent_crimes
 
 test_result = unittest.TextTestRunner().run(unittest.TestLoader().loadTestsFromModule(Testing.TestFunction1))
 
+y = violent_crimes
 # Plotting
 plt.figure()
-plt.plot(video_gamer, violent_crimes, 'o')
+plt.plot(x, y, 'o')
 
 if test_result.wasSuccessful():
     # Linear regression if the test is successful
-    lin_reg_m_main, lin_reg_q_main = linear_regression(video_gamer, violent_crimes)
-    y_main = lin_reg_m_main * video_gamer + lin_reg_q_main
-    plt.plot(video_gamer, y_main, label="Regression line")
+    lin_reg_m_main, lin_reg_q_main = linear_regression(x, y)
+    y_main = lin_reg_m_main * x+ lin_reg_q_main
+    plt.plot(x, y_main, label="Regression line")
     # Calculate and display PMCC
-    plt.text(0.75, 1.035, f"PMCC: {PmCc(video_gamer, violent_crimes):.2f} > Successful Correlation", transform=plt.gca().transAxes, fontsize=5, verticalalignment='top')
+    plt.text(0.75, -0.09, f"PMCC: {PmCc(x, y):.2f} > Successful Correlation", transform=plt.gca().transAxes, fontsize=5, verticalalignment='top')
 else:
     # Display a message if the test is not successful
-    plt.text(0.75, 1.035, f"PMCC: {PmCc(video_gamer, violent_crimes):.2f} > No Successful Correlation", transform=plt.gca().transAxes, fontsize=5, verticalalignment='top')
+    plt.text(0.75, -0.09, f"PMCC: {PmCc(x, y):.2f} > No Successful Correlation", transform=plt.gca().transAxes, fontsize=5, verticalalignment='top')
 
 plt.xlabel("Video gaming rate (%)")
-plt.ylabel("Weapon possessions")
-plt.title("Adult video gaming and weapon possessions correlation")
+plt.ylabel("Number of Violent crimes")
+plt.title("Adult video gaming and violent crime correlation")
+
+plt.legend()
+
+# Display the plot
+plt.show()
+
+test_result = unittest.TextTestRunner().run(unittest.TestLoader().loadTestsFromModule(Testing.TestFunction1))
+
+y = sexual_offences
+# Plotting
+plt.figure()
+plt.plot(x, y, 'o')
+
+if test_result.wasSuccessful():
+    # Linear regression if the test is successful
+    lin_reg_m_main, lin_reg_q_main = linear_regression(x, y)
+    y_main = lin_reg_m_main * x+ lin_reg_q_main
+    plt.plot(x, y_main, label="Regression line")
+    # Calculate and display PMCC
+    plt.text(0.75, -0.09, f"PMCC: {PmCc(x, y):.2f} > Successful Correlation", transform=plt.gca().transAxes, fontsize=5, verticalalignment='top')
+else:
+    # Display a message if the test is not successful
+    plt.text(0.75, -0.09, f"PMCC: {PmCc(x, y):.2f} > No Successful Correlation", transform=plt.gca().transAxes, fontsize=5, verticalalignment='top')
+
+plt.xlabel("Video gaming rate (%)")
+plt.ylabel("Number of Sexual offences")
+plt.title("Adult video gaming and sexual offences correlation")
+
+plt.legend()
+
+# Display the plot
+plt.show()
+
+test_result = unittest.TextTestRunner().run(unittest.TestLoader().loadTestsFromModule(Testing.TestFunction1))
+
+y = weapon_possession
+# Plotting
+plt.figure()
+plt.plot(x, y, 'o')
+
+if test_result.wasSuccessful():
+    # Linear regression if the test is successful
+    lin_reg_m_main, lin_reg_q_main = linear_regression(x, y)
+    y_main = lin_reg_m_main * x+ lin_reg_q_main
+    plt.plot(x, y_main, label="Regression line")
+    # Calculate and display PMCC
+    plt.text(0.75, -0.09, f"PMCC: {PmCc(x, y):.2f} > Successful Correlation", transform=plt.gca().transAxes, fontsize=5, verticalalignment='top')
+else:
+    # Display a message if the test is not successful
+    plt.text(0.75, -0.09, f"PMCC: {PmCc(x, y):.2f} > No Successful Correlation", transform=plt.gca().transAxes, fontsize=5, verticalalignment='top')
+
+plt.xlabel("Video gaming rate (%)")
+plt.ylabel("Number of Weapon possession")
+plt.title("Adult video gaming and weapon possession correlation")
+
+plt.legend()
+
+# Display the plot
+plt.show()
+
+test_result = unittest.TextTestRunner().run(unittest.TestLoader().loadTestsFromModule(Testing.TestFunction1))
+
+y = suicide_rate
+# Plotting
+plt.figure()
+plt.plot(x, y, 'o')
+
+if test_result.wasSuccessful():
+    # Linear regression if the test is successful
+    lin_reg_m_main, lin_reg_q_main = linear_regression(x, y)
+    y_main = lin_reg_m_main * x+ lin_reg_q_main
+    plt.plot(x, y_main, label="Regression line")
+    # Calculate and display PMCC
+    plt.text(0.75, -0.09, f"PMCC: {PmCc(x, y):.2f} > Successful Correlation", transform=plt.gca().transAxes, fontsize=5, verticalalignment='top')
+else:
+    # Display a message if the test is not successful
+    plt.text(0.75, -0.09, f"PMCC: {PmCc(x, y):.2f} > No Successful Correlation", transform=plt.gca().transAxes, fontsize=5, verticalalignment='top')
+
+plt.xlabel("Video gaming rate (%)")
+plt.ylabel("Suicide rate")
+plt.title("Adult video gaming and suicide rate correlation")
+
+plt.legend()
+
+# Display the plot
+plt.show()
+
+x = gamingtwelve_rate
+
+test_result = unittest.TextTestRunner().run(unittest.TestLoader().loadTestsFromModule(Testing.TestFunction1))
+
+y = homicide_rate
+# Plotting
+plt.figure()
+plt.plot(x, y, 'o')
+
+if test_result.wasSuccessful():
+    # Linear regression if the test is successful
+    lin_reg_m_main, lin_reg_q_main = linear_regression(x, y)
+    y_main = lin_reg_m_main * x+ lin_reg_q_main
+    plt.plot(x, y_main, label="Regression line")
+    # Calculate and display PMCC
+    plt.text(0.75, -0.09, f"PMCC: {PmCc(x, y):.2f} > Successful Correlation", transform=plt.gca().transAxes, fontsize=5, verticalalignment='top')
+else:
+    # Display a message if the test is not successful
+    plt.text(0.75, -0.09, f"PMCC: {PmCc(x, y):.2f} > No Successful Correlation", transform=plt.gca().transAxes, fontsize=5, verticalalignment='top')
+
+plt.xlabel("Video gaming rate (%)")
+plt.ylabel("Homicide rate")
+plt.title("Adult video gaming and homicide rate correlation")
 
 plt.legend()
 
--- variables.py
+++ variables.py
@@ -23,6 +23,22 @@ print(video_gamer)
 data_dates = np.array(data_dates1)
 print(data_dates)
 
+df = pd.read_excel("statistic_id283093_number-of-homicides-in-england-and-wales-2002-2022.xlsx", "Data2", header = 2)
+df.head()
+print(df)
+
+z = dataframe_to_dictionary(df)
+twelve_dates1 = z["Year"]
+
+z = dataframe_to_dictionary(df)
+gamingtwelve_rate1 = z["Games"]
+
+twelve_dates = np.array(twelve_dates1)
+print(twelve_dates)
+
+gamingtwelve_rate = np.array(gamingtwelve_rate1)
+print(gamingtwelve_rate)
+
 df = pd.read_excel("statistic_id288256_number-of-violent-crime-offences-in-england-and-wales-2007-2022.xlsx", "Data", header = 3)
 df.head()
 print(df)
@@ -30,11 +46,51 @@ print(df)
 x = dataframe_to_dictionary(df)
 violent_crimes1 = x["Crimes"]
 
-z = dataframe_to_dictionary(df)
-data_dates1 = z["Date"]
-
 violent_crimes = np.array(violent_crimes1)
 print(violent_crimes)
 
-data_dates = np.array(data_dates1)
-print(data_dates)
+df = pd.read_excel("Figure_10__Suspects_convicted_of_homicide_show_a_younger_age_profile (2).xlsx", "Data5", header = 2)
+df.head()
+print(df)
+
+y = dataframe_to_dictionary(df)
+sexual_offences1 = y["Sexual Offences"]
+
+sexual_offences = np.array(sexual_offences1)
+print(sexual_offences)
+
+df = pd.read_excel("statistic_id828895_number-of-possession-of-weapon.xlsx", "Data", header = 2)
+df.head()
+print(df)
+
+y = dataframe_to_dictionary(df)
+weapon_possession1 = y["weapon possession"]
+
+weapon_possession = np.array(weapon_possession1)
+print(weapon_possession)
+
+df = pd.read_excel("statistic_id283093_number-of-homicides-in-england-and-wales-2002-2022.xlsx", "Data", header = 1)
+df.head()
+print(df)
+
+z = dataframe_to_dictionary(df)
+twelve_dates1 = z["Year"]
+
+z = dataframe_to_dictionary(df)
+homicide_rate1 = z["Number of homicides in England and Wales 2002-2022"]
+
+twelve_dates = np.array(twelve_dates1)
+print(twelve_dates)
+
+homicide_rate = np.array(homicide_rate1)
+print(homicide_rate)
+
+df = pd.read_excel("statistic_id282160_suicide-rate-in-england-and-wales-2000-2021 (1).xlsx", "Data", header = 3)
+df.head()
+print(df)
+
+y = dataframe_to_dictionary(df)
+suicide_rate1 = y["Suicide rate "]
+
+suicide_rate = np.array(suicide_rate1)
+print(suicide_rate)
