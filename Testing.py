cc6f18b361ec4fa1d6d1116ec210a45060470075
ec221020 <ec221020@qmul.ac.uk>
2023-12-12 13:41:38 +0000

    *New CircleCi

--- Testing.py
+++ Testing.py
@@ -2,71 +2,54 @@ import unittest
 from functions import PmCc
 from variables import video_gamer, gamingtwelve_rate, data_dates, violent_crimes, sexual_offences, weapon_possession,suicide_rate,homicide_rate
 
-x = video_gamer
-y = violent_crimes
-
 class TestFunction1(unittest.TestCase):
 
-    def test_addition_lower_one(self):
-            result = PmCc(x, y)
+    def test_addition_lower_one1(self):
+            result = PmCc(video_gamer, violent_crimes)
             self.assertLessEqual(abs(result), 1)
 
 
-    def test_addition_greater_point_one(self):
-            result = PmCc(x, y)
+    def test_addition_greater_point_one1(self):
+            result = PmCc(video_gamer, violent_crimes)
             self.assertGreaterEqual(abs(result), 0.1)
 
-y = sexual_offences
-
-class TestFunction2(unittest.TestCase):
-
-    def test_addition_lower_one(self):
-            result = PmCc(x, y)
+    def test_addition_lower_one2(self):
+            result = PmCc(video_gamer, sexual_offences)
             self.assertLessEqual(abs(result), 1)
 
 
-    def test_addition_greater_point_one(self):
-            result = PmCc(x, y)
+    def test_addition_greater_point_one2(self):
+            result = PmCc(video_gamer, sexual_offences)
             self.assertGreaterEqual(abs(result), 0.1)
 
-y = weapon_possession
 
-class TestFunction3(unittest.TestCase):
-
-    def test_addition_lower_one(self):
-            result = PmCc(x, y)
+    def test_addition_lower_one3(self):
+            result = PmCc(video_gamer, weapon_possession)
             self.assertLessEqual(abs(result), 1)
 
 
-    def test_addition_greater_point_one(self):
-            result = PmCc(x, y)
+    def test_addition_greater_point_one3(self):
+            result = PmCc(video_gamer, weapon_possession)
             self.assertGreaterEqual(abs(result), 0.1)
 
-y = suicide_rate
 
-class TestFunction4(unittest.TestCase):
-
-    def test_addition_lower_one(self):
-            result = PmCc(x, y)
+    def test_addition_lower_one4(self):
+            result = PmCc(video_gamer, suicide_rate)
             self.assertLessEqual(abs(result), 1)
 
 
-    def test_addition_greater_point_one(self):
-            result = PmCc(x, y)
+    def test_addition_greater_point_one4(self):
+            result = PmCc(video_gamer, suicide_rate)
             self.assertGreaterEqual(abs(result), 0.1)
 
-x = gamingtwelve_rate
-y = homicide_rate
 
-class TestFunction5(unittest.TestCase):
-
-    def test_addition_lower_one(self):
-            result = PmCc(x, y)
+    def test_addition_lower_one5(self):
+            result = PmCc(gamingtwelve_rate, homicide_rate)
             self.assertLessEqual(abs(result), 1)
 
 
-    def test_addition_greater_point_one(self):
-            result = PmCc(x, y)
+    def test_addition_greater_point_one5(self):
+            result = PmCc(gamingtwelve_rate, homicide_rate)
             self.assertGreaterEqual(abs(result), 0.1)
 
 
